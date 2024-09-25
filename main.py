305af1b0f208ebe81e1ddcd7f4ba8a144bc801e1
lucamiras <lucamiras@googlemail.com>
2024-09-25 22:37:31 +0200

    Implemented streamlit interface

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from langchain.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from src.agents import Agent
 from src.prompts import raspberry_agents
+import json
 
 
 agent_1 = Agent(
@@ -16,7 +17,28 @@ agent_2 = Agent(
     prompt=raspberry_agents["Agent2"],
 )
 
-user_prompt = "Hi Agent, could you count the letters 'r' in the word raspberry?"
+agent_3 = Agent(
+    name="Agent3",
+    prompt=raspberry_agents["Agent3"],
+)
 
-response_1 = agent_1.deploy_agent(user_prompt)
-response_2 = agent_2.deploy_agent(f"What the user initially asked: {user_prompt}. What Agent 1 proposes: {response_1}")
+headline = st.header("Chain of thought reasoning")
+sidebar = st.sidebar
+user_prompt = st.text_input("User prompt", "Hi Agent, could you count the letters 'r' in the word raspberry?")
+button = st.button("Start reasoning")
+response = st.container()
+
+if button:
+    response_1 = agent_1.deploy_agent(user_prompt)
+    sidebar.title("Agent 1 response:")
+    sidebar.markdown(f"Agent 1 response: {response_1}")
+
+    response_2 = agent_2.deploy_agent(f"What the user initially asked: {user_prompt}. What Agent 1 proposes: {response_1}")
+    sidebar.title("Agent 2 response:")
+    sidebar.markdown(f"Agent 2 response: {response_2}")
+    
+    response_3 = agent_3.deploy_agent(f"What Agent 2 proposes: {response_2}")
+    sidebar.title("Agent 3 response:")
+    sidebar.markdown(f"Agent 3 response: {response_3}")
+
+    response.write(response_3)
--- src/prompts.py
+++ src/prompts.py
@@ -27,6 +27,16 @@ raspberry_agents = {
         - Agent, any response you give MUST come in JSON format with the following keys: \n
         - "problem": The problem statement. \n
         - "solution": The detailed solution to the problem including every reasoning step. \n
+        - "final_output": The final output, summarized in one sentence. Only applicable if "next action" is "final_answer"\n
         - "next_action": "continue" to proceed with more steps or "final_answer" if you are confident in your solution. \n
+    """,
+    "Agent3":
+    """
+        You are the third agent, the only agent who can provide the final answer. \n
+        Your mission is to provide the final answer to the problem statement. \n
+        Below you will find the output from Agent 2, who has critically analyzed the solution provided by Agent 1. \n
+        The final solution can be found in the JSON output from Agent 2 with the key "final_output". \n
+        Reformulate the final answer in a clear and concise manner. Write a single sentence. \n
+        It is of UTMOST important that you ONLY provide the final answer. Do not comment on it in any other way. The mission will fail if you do! 
     """
 }
